02f911d6fbf936394f8d25b6484afcd0dcf13992
Naufal Adifa Ramdani <101171611+gopalllll@users.noreply.github.com>
2022-10-16 15:16:42 +0700

    Add files via upload

---
+++ pert1 praktikum/pert1.js
@@ -0,0 +1,8 @@
+
+var umur = 17;
+
+var nama = "nama saya naufal adifa ramdani";
+
+console.log (nama);
+
+console.log (`umurnya ${17} tahun`);
---
+++ pert1 praktikum/pert1b.js
@@ -0,0 +1,13 @@
+var manusia = [{
+    nama: "Naufal",
+    umur: 20,
+    hobi: "Tidur dan main game",
+    wargaIndonesia: true,
+},{ 
+    nama: "Gopal",
+    umur: 21,
+    hobi: "Belajar sambil mengantuk",
+    wargaIndonesia: false,
+}];
+
+console.log(manusia);
